eef0f491b0e0abb2ade341fdb6f3953bb25da004
ksproska <kamila.sproska@gmail.com>
2021-10-16 20:43:37 +0200

    quickfix - static arguments kopie funkcji niezbędn

--- L02_01/L02_01.ino
+++ L02_01/L02_01.ino
@@ -97,19 +97,123 @@ bool isButtonReliesed(int button)
 }
 
 bool isGreenButtonPressed() {
-  return isButtonPressed(GREEN_BUTTON);
+  static int debounced_button_state = HIGH;
+    static int previous_reading = HIGH;
+    static unsigned long last_change_time = 0UL;
+    bool isPressed = false;
+
+    int current_reading = digitalRead(GREEN_BUTTON);
+
+    if (previous_reading != current_reading)
+    {
+        last_change_time = millis();
+    }
+
+    if (millis() - last_change_time > DEBOUNCE_PERIOD)
+    {
+        if (current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+
+    previous_reading = current_reading;
+
+    return isPressed;
 }
 
 bool isGreenButtonReliesed() {
-  return isButtonReliesed(GREEN_BUTTON);
+  static int debounced_button_state = LOW;
+    static int previous_reading = LOW;
+    static unsigned long last_change_time = 0UL;
+    bool isReliesed = false;
+
+    int current_reading = digitalRead(GREEN_BUTTON);
+
+    if (previous_reading != current_reading)
+    {
+        last_change_time = millis();
+    }
+
+    if (millis() - last_change_time > DEBOUNCE_PERIOD)
+    {
+        if (current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == LOW && current_reading == HIGH)
+            {
+                isReliesed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+
+    previous_reading = current_reading;
+
+    return isReliesed;
 }
 
 bool isRedButtonPressed() {
-  return isButtonPressed(RED_BUTTON);
+  static int debounced_button_state = HIGH;
+    static int previous_reading = HIGH;
+    static unsigned long last_change_time = 0UL;
+    bool isPressed = false;
+
+    int current_reading = digitalRead(RED_BUTTON);
+
+    if (previous_reading != current_reading)
+    {
+        last_change_time = millis();
+    }
+
+    if (millis() - last_change_time > DEBOUNCE_PERIOD)
+    {
+        if (current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+
+    previous_reading = current_reading;
+
+    return isPressed;
 }
 
 bool isRedButtonReliesed() {
-  return isButtonReliesed(RED_BUTTON);
+  static int debounced_button_state = LOW;
+    static int previous_reading = LOW;
+    static unsigned long last_change_time = 0UL;
+    bool isReliesed = false;
+
+    int current_reading = digitalRead(RED_BUTTON);
+
+    if (previous_reading != current_reading)
+    {
+        last_change_time = millis();
+    }
+
+    if (millis() - last_change_time > DEBOUNCE_PERIOD)
+    {
+        if (current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == LOW && current_reading == HIGH)
+            {
+                isReliesed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+
+    previous_reading = current_reading;
+
+    return isReliesed;
 }
 
 bool prev_green = false;
