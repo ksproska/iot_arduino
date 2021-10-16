74d5a7a730fdd783494d25b0861d3fd623089177
ksproska <kamila.sproska@gmail.com>
2021-10-16 20:34:20 +0200

    init

---
+++ L02_01/L02_01.ino
@@ -0,0 +1,166 @@
+//Napisz program, Program, po naciśnięciu i zwolnieniu jednego z przycisków 
+//(zielony i czerwony) ma zgasić zaświeconą diodę
+//i zapalić kolejną dokładnie w cyklu R,G,B,R,G,...
+
+#define LED_RED 6
+#define LED_GREEN 5
+#define LED_BLUE 3
+
+#define RED_BUTTON 2
+#define GREEN_BUTTON 4
+
+int led[] = {LED_RED, LED_GREEN, LED_BLUE};
+
+void initRGB()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    pinMode(LED_RED, OUTPUT);
+    digitalWrite(LED_RED, HIGH);
+
+    pinMode(LED_GREEN, OUTPUT);
+    digitalWrite(LED_GREEN, LOW);
+
+    pinMode(LED_BLUE, OUTPUT);
+    digitalWrite(LED_BLUE, LOW);
+}
+
+void initButtons()
+{
+    pinMode(RED_BUTTON, INPUT_PULLUP);
+    pinMode(GREEN_BUTTON, INPUT_PULLUP);
+}
+
+#define DEBOUNCE_PERIOD 10UL
+
+bool isButtonPressed(int button)
+{
+    static int debounced_button_state = HIGH;
+    static int previous_reading = HIGH;
+    static unsigned long last_change_time = 0UL;
+    bool isPressed = false;
+
+    int current_reading = digitalRead(button);
+
+    if (previous_reading != current_reading)
+    {
+        last_change_time = millis();
+    }
+
+    if (millis() - last_change_time > DEBOUNCE_PERIOD)
+    {
+        if (current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+
+    previous_reading = current_reading;
+
+    return isPressed;
+}
+
+bool isButtonReliesed(int button)
+{
+    static int debounced_button_state = LOW;
+    static int previous_reading = LOW;
+    static unsigned long last_change_time = 0UL;
+    bool isReliesed = false;
+
+    int current_reading = digitalRead(button);
+
+    if (previous_reading != current_reading)
+    {
+        last_change_time = millis();
+    }
+
+    if (millis() - last_change_time > DEBOUNCE_PERIOD)
+    {
+        if (current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == LOW && current_reading == HIGH)
+            {
+                isReliesed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+
+    previous_reading = current_reading;
+
+    return isReliesed;
+}
+
+bool isGreenButtonPressed() {
+  return isButtonPressed(GREEN_BUTTON);
+}
+
+bool isGreenButtonReliesed() {
+  return isButtonReliesed(GREEN_BUTTON);
+}
+
+bool isRedButtonPressed() {
+  return isButtonPressed(RED_BUTTON);
+}
+
+bool isRedButtonReliesed() {
+  return isButtonReliesed(RED_BUTTON);
+}
+
+bool prev_green = false;
+bool prev_red = false;
+
+bool wasGreenPressedAndReliesed() {
+  if (isGreenButtonPressed()) {
+    prev_green = true;
+  }
+  if (prev_green & isGreenButtonReliesed()) {
+    prev_green = false;
+    return true;
+  }
+  return false;
+}
+
+bool wasRedPressedAndReliesed() {
+  if (isRedButtonPressed()) {
+    prev_red = true;
+  }
+  if (prev_red & isRedButtonReliesed()) {
+    prev_red = false;
+    return true;
+  }
+  return false;
+}
+
+int led_index = 0;
+
+void onNextColor() {
+  digitalWrite(led[led_index], LOW);
+  led_index = ++led_index % 3;
+  digitalWrite(led[led_index], HIGH);
+}
+
+void setup()
+{
+    initRGB();
+    initButtons();
+    digitalWrite(led[led_index], HIGH);
+}
+
+void loop()
+{
+    // działają osobno ale nie razem
+    if (wasGreenPressedAndReliesed())
+    {
+        onNextColor();
+    }
+    if (wasRedPressedAndReliesed())
+    {
+        onNextColor();
+    }
+}
