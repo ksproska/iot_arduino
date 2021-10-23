e2bde9e3597470690a6761d39346c4087f499ade
ksproska <kamila.sproska@gmail.com>
2021-10-23 14:48:45 +0200

    works in simulator

--- L03_03/L03_03.ino
+++ L03_03/L03_03.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+// Napisz program, który dla napięcia zadanego potencjometrem, na wyświetlaczu LCD wyświetli 
+// wartość, którą odczytał ADC i rzeczywistą wartość napięcia zadanego potencjometrem.
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+#define POTENTIOMETER A0
 
+void initLCD()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    initLCD();
+}
 
+void displayInt(int int_val) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(int_val);
+}
+
+int value = -1;
+void loop()
+{
+    int value_new = analogRead(POTENTIOMETER);
+    if(value != value_new) {
+      value = value_new;
+      displayInt(value);
+    }
 }
