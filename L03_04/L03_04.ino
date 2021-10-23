30b0df49b4f7ca94ec389f0a107ccd3d01c2ed0e
ksproska <kamila.sproska@gmail.com>
2021-10-23 14:54:53 +0200

    works in simulatror

--- L03_04/L03_04.ino
+++ L03_04/L03_04.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+// Napisz program, który odczytuje wartość z przetwornika analogowo-cyfrowego, 
+// a następnie wysyła przez łącze szeregowe do komputera. Zobacz wynik jego działania 
+// przy pomocy narzędzi Serial Monitor i Serial Plotter z Arduino IDE.
 
+#define POTENTIOMETER A0
+
+void initSerial() {
+    Serial.begin(9600);
+    while (!Serial) {}
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() {
+    initSerial();
+}
 
+
+void loop()
+{
+    int value = analogRead(POTENTIOMETER);
+    String str = String(value);
+    Serial.println(str);
 }
