aca5382524059d3d3a16309d4b38474ab32a2a0d
ksproska <kamila.sproska@gmail.com>
2021-11-02 20:04:54 +0100

    init

---
+++ L05_02/L05_02.ino
@@ -0,0 +1,14 @@
+// Przygotuj program, który będzie pozwalał sterować świeceniem diody RGB. Program ma być 
+// wyposażony w przewijalne menu na ekranie wyświetlacza LCD (w kolejnych liniach wyświetlane 
+// są dwie pozycje z menu). Samodzielnie zaprojektuj strukturę menu programu. Nawigacja po 
+// menu (przemieszczanie się pomiędzy pozycjami) ma odbywać się za pomocą enkodera. Wybór
+// pozycji menu odbywa się za pomocą przycisku. Minimalna funkcjonalność to zapalanie i 
+// gaszenie wybranej diody. Można też zaimplementować ustawianie jasności każdego kolory 
+// diody enkoderem. Warto też program oprzeć na przerwaniach zgłaszanych przy zmianie stanu enkodera.
+
+void setup() {
+}
+
+
+void loop() {
+}
