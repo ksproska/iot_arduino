7057e58c95cfbcd78da0f759cb8b2cf66476099f
ksproska <kamila.sproska@gmail.com>
2021-11-11 14:14:12 +0100

    excercises description

--- L06_1/L06_1.ino
+++ L06_1/L06_1.ino
@@ -1,3 +1,12 @@
+// Zadanie 1: Program wykorzystujący czujniki DS18B20
+
+// Przygotuj program, który uczyni zestaw laboratoryjny małą „stacją pogodową”. 
+// Na wyświetlaczu LCD ma prezentować temperaturę wewnętrzną (czujnik wewnątrz zestawu) i 
+// zewnętrzną (sonda na przewodzie). Ponadto program powinien pamiętać wartość maksymalną i 
+// minimalną temperatury mierzonej przez sondę na przewodzie. Dodatkowo, dioda RGB, może
+// sygnalizować czy jesteśmy w streﬁe komfortu temperaturowego, lub jest za gorąco, lub za zimno.
+// Jest to zadanie przykładowe. Prowadzący może zmodyﬁkować lub zmienić treść zadania.
+
 void setup() {
     // setup
 }
--- L06_2/L06_2.ino
+++ L06_2/L06_2.ino
@@ -1,3 +1,13 @@
+// Zadanie 2: Implementacja własnej biblioteki ze sterownikiem wybranego urządzenia
+
+// Przygotuj własną bibliotekę, która będzie służyła do sterowania diodą RGB. 
+// Samodzielnie zaprojektuj interfejs programistyczny tej biblioteki, tak aby pozwalał podać, 
+// do których pinów podłączona jest dioda, oraz aby pozwalał ustawiać kolory
+// poprzez podanie wartości komponentów RGB oraz przez nazwy kolorów: 
+// RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA, BLACK, WHITE. 
+// Zadbaj o kolorowanie składni w Arduino IDE.
+// Jest to zadanie przykładowe. Prowadzący może zmodyﬁkować lub zmienić treść zadania.
+
 void setup() {
     // setup
 }
