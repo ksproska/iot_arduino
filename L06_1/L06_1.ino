61de17d4f70d3be43f400609694093e4cf81b4f5
ksproska <kamila.sproska@gmail.com>
2021-11-12 12:52:23 +0100

    first draft

--- L06_1/L06_1.ino
+++ L06_1/L06_1.ino
@@ -7,10 +7,88 @@
 // sygnalizować czy jesteśmy w streﬁe komfortu temperaturowego, lub jest za gorąco, lub za zimno.
 // Jest to zadanie przykładowe. Prowadzący może zmodyﬁkować lub zmienić treść zadania.
 
-void setup() {
-    // setup
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+OneWire oneWire(A1);
+DallasTemperature tempSensors(&oneWire);
+
+#define LED_RED 6
+#define LED_GREEN 5
+#define LED_BLUE 3
+int led[] = {LED_RED, LED_GREEN, LED_BLUE};
+
+void initRGB() {
+    pinMode(LED_RED, OUTPUT);
+    digitalWrite(LED_RED, LOW);
+
+    pinMode(LED_GREEN, OUTPUT);
+    digitalWrite(LED_GREEN, LOW);
+
+    pinMode(LED_BLUE, OUTPUT);
+    digitalWrite(LED_BLUE, LOW);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
-    // loop
+void setup()
+{   
+    initRGB();
+    tempSensors.begin();
+
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+}
+
+bool areWeInComfortZone(float tempOut) {
+    return 22.0 < tempOut && tempOut < 27.0;
+}
+
+float tempIn = null;
+float tempOut = null;
+float tempOutMax = tempOut;
+float tempOutMin = tempOut;
+
+void updateMaxMin() {
+    tempSensors.requestTemperatures();
+    tempIn = tempSensors.getTempCByIndex(1);
+    tempOut = tempSensors.getTempCByIndex(0);
+    
+    if (tempOutMax == null || tempOut > tempOutMax) {
+        tempOutMax = tempOut;
+    }
+
+    if (tempOutMin == null || tempOut < tempOutMin) {
+        tempOutMin = tempOut;
+    }
+}
+
+void displayTemperatures() {
+    char buffer[40];
+    sprintf(buffer, "Temp  IN%8s", String(tempIn, 4).c_str());
+    lcd.setCursor(0, 0);
+    lcd.print(buffer);
+
+    sprintf(buffer, "Temp OUT%8s", String(tempOut, 4).c_str());
+    lcd.setCursor(0, 1);
+    lcd.print(buffer);
+}
+
+void loop()
+{
+    updateMaxMin();
+    displayTemperatures();
+    
+    if(areWeInComfortZone(tempOut)) {
+        digitalWrite(LED_GREEN, HIGH);
+    }
+    else {
+        digitalWrite(LED_GREEN, LOW);
+    }
 }
